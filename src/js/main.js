d271e25d7626af990ca6601270a2cb6a16718d7e
Vlad Chernov <vladd.chernov@gmail.com>
2021-01-12 11:07:16 +0200

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -11,12 +11,12 @@ module.exports = class MainJs {
   // ANIMATION FOR THE FIRST BANNER
   bannerFirstOptionAnimation() {
     const animator = {};
-    const ANIMATION_SPEED = 2000;
+    const ANIMATION_SPEED = 1000;
     const SLIDE_ANIMATION_SPEED = 400;
     const ANIMATION_DELAY = 2000;
     const ANIMATION_DELAY_MEDIUM = 500;
     const ANIMATION_DELAY_SMALL = 200;
-    const ANIMATION_DELAY_READ = 8000;
+    const ANIMATION_DELAY_READ = 4000;
     // reference variables to DOM elements
     const isi = document.querySelector('.isi-container-wrapper');
     const screen1 = document.querySelector('.screen-1');
@@ -28,23 +28,26 @@ module.exports = class MainJs {
 
     const frame1 = () => {
     
-      helperFunctions.fadeIn(screen1.querySelector('img'), ANIMATION_SPEED, () => {
-        helperFunctions.fadeOut(screen1.querySelector('img'), ANIMATION_SPEED, () => {
+      helperFunctions.fadeIn(screen1.querySelector('img'), SLIDE_ANIMATION_SPEED, () => {
+        setTimeout(() => {
+        helperFunctions.fadeOut(screen1.querySelector('img'), SLIDE_ANIMATION_SPEED, () => {
           setTimeout(() => {
             helperFunctions.animate(screen1, {
               opacity: 0
-            }, ANIMATION_SPEED, 'ease');
+            }, ANIMATION_DELAY, 'ease');
 
             frame2();
           }, ANIMATION_DELAY_SMALL);
+        
         });
+      }, ANIMATION_DELAY, 'ease');
       });
     };
 
     const frame2 = () => {
-      helperFunctions.fadeIn(screen2, ANIMATION_SPEED, () => {
+      helperFunctions.fadeIn(screen2, SLIDE_ANIMATION_SPEED, () => {
         setTimeout(() => {
-          helperFunctions.fadeOut(screen2, ANIMATION_SPEED, () => {
+          helperFunctions.fadeOut(screen2, SLIDE_ANIMATION_SPEED, () => {
             setTimeout(() => {
   
               frame3();
@@ -58,51 +61,56 @@ module.exports = class MainJs {
     const frame3 = () => {
       helperFunctions.animate(screen3, {display: 'block'}, 0, 'ease');
       helperFunctions.fadeIn(screen3.querySelector('h1'), SLIDE_ANIMATION_SPEED);
-      helperFunctions.fadeIn(screen3.querySelector('.screen-bg'), 4000, () => {
-      helperFunctions.fadeOut(screen3.querySelector('h1'), SLIDE_ANIMATION_SPEED);
+      setTimeout(() => {
+      helperFunctions.fadeIn(screen3.querySelector('.screen-bg'), ANIMATION_SPEED, () => {
+        setTimeout(() => {
+        helperFunctions.fadeOut(screen3.querySelector('h1'), SLIDE_ANIMATION_SPEED);
       setTimeout(() => {
         helperFunctions.animate(screen3, {display: 'none'}, 0, 'ease');
         frame4();
       }, SLIDE_ANIMATION_SPEED + 50);
+    }, ANIMATION_DELAY, 'ease');
       });
+    }, SLIDE_ANIMATION_SPEED);
     };
 
     const frame4 = () => {
-      helperFunctions.animate(isi, {display:'block'}, 0, 'ease');
+      
       helperFunctions.animate(screen4, {transform: 'translateY(0)'}, SLIDE_ANIMATION_SPEED, 'ease', () => {
         setTimeout(() => {
           helperFunctions.fadeIn(screen4.querySelector('h1'), SLIDE_ANIMATION_SPEED);
           setTimeout(() => {
+            helperFunctions.animate(isi, {display:'block'}, 0, 'ease');
           helperFunctions.animate(isi, {transform: 'translateY(0)'}, SLIDE_ANIMATION_SPEED, 'ease');
-         
-          setTimeout(() => {
-            this.isi.init();
-            this.isi.refresh();
+          this.isi.init();
+          this.isi.refresh();
             setTimeout(() => {
             helperFunctions.fadeOut(screen4.querySelector('h1'), SLIDE_ANIMATION_SPEED);
-          },ANIMATION_DELAY - 400);
+            }, SLIDE_ANIMATION_SPEED);
             setTimeout(() => {
               helperFunctions.animate(screen4, {transform: 'translateY(-100%)'}, SLIDE_ANIMATION_SPEED, 'ease');
-  
               frame5();
-            }, ANIMATION_DELAY );
-          },ANIMATION_DELAY);
-        }, ANIMATION_DELAY_SMALL);
+            }, 800);
+        }, ANIMATION_DELAY);
         }, ANIMATION_DELAY_SMALL);
       });
     };
 
     const frame5 = () => {
-      helperFunctions.animate(screen5, {transform: 'translateY(0)'}, SLIDE_ANIMATION_SPEED, 'ease', () => {
+        helperFunctions.animate(screen5, {transform: 'translateY(0)'}, SLIDE_ANIMATION_SPEED, 'ease', () => {
+        setTimeout(() => {
         helperFunctions.fadeIn(screen5.querySelector('h1'), SLIDE_ANIMATION_SPEED);
         setTimeout(() => {
-          helperFunctions.animate(screen5.querySelector('h1'), {opacity: 0}, SLIDE_ANIMATION_SPEED, 'ease');
-          helperFunctions.animate(screen5, {transform: 'translateY(100%)'}, SLIDE_ANIMATION_SPEED, 'ease', () => {
+          helperFunctions.fadeOut(screen5.querySelector('h1'), SLIDE_ANIMATION_SPEED);
+          setTimeout(() => {
+          // helperFunctions.animate(screen5, {transform: 'translateY(100%)'}, SLIDE_ANIMATION_SPEED, 'ease', () => {
 
             frame6();
-          });
+          // });
+        }, SLIDE_ANIMATION_SPEED);
         }, ANIMATION_DELAY);
-      });
+    }, SLIDE_ANIMATION_SPEED);
+  });
     };
 
 
@@ -116,14 +124,13 @@ module.exports = class MainJs {
           setTimeout(() => {
             helperFunctions.animate(screen6.querySelector('.button'), {transform: 'translateX(0)'}, SLIDE_ANIMATION_SPEED, 'ease');
             helperFunctions.animate(screen1, {opacity: 1}, 0, 'ease');
-            setTimeout(() => {
-              helperFunctions.fadeOut(screen6, 0);
-              helperFunctions.animate(screen6.querySelector('.bg-image'), {transform: 'translateY(-100%)'}, SLIDE_ANIMATION_SPEED, 'ease');
-              helperFunctions.animate(screen6.querySelector('.logo-image'), {transform: 'translateX(250%)'}, SLIDE_ANIMATION_SPEED, 'ease');
-              helperFunctions.animate(screen6.querySelector('.button'), {transform: 'translateX(250%)'}, SLIDE_ANIMATION_SPEED, 'ease');
-              helperFunctions.animate(isi, {display:'none'}, 0, 'ease');
-              frame1();
-          }, ANIMATION_DELAY);
+          //   setTimeout(() => {
+          //     helperFunctions.fadeOut(screen6, 0);
+          //     helperFunctions.animate(screen6.querySelector('.bg-image'), {transform: 'translateY(-100%)'}, SLIDE_ANIMATION_SPEED, 'ease');
+          //     helperFunctions.animate(screen6.querySelector('.logo-image'), {transform: 'translateX(250%)'}, SLIDE_ANIMATION_SPEED, 'ease');
+          //     helperFunctions.animate(screen6.querySelector('.button'), {transform: 'translateX(250%)'}, SLIDE_ANIMATION_SPEED, 'ease');
+          //     helperFunctions.animate(isi, {display:'none'}, 0, 'ease');
+          // }, ANIMATION_DELAY);
           }, ANIMATION_DELAY_SMALL);
         }, ANIMATION_DELAY_SMALL);
       }, ANIMATION_DELAY_SMALL);
