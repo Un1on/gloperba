9884f2feccfc56b9558fb66814142d51fa06a540
Vlad Chernov <vladd.chernov@gmail.com>
2021-01-28 22:59:20 +0200

    fixes

--- src/js/components/exit-links.js
+++ src/js/components/exit-links.js
@@ -5,17 +5,37 @@ module.exports = function() {
       links[i].addEventListener('click', e => {
         e.preventDefault();
         e.stopPropagation();
-        Enabler.exit('Gloperba', 'http://gloperba.com/');
+        Enabler.exit('Gloperba', 'http://gloperba.com');
       });
     }
   }
-  if (document.getElementsByClassName('linkToHowWorksPatient').length) {
-    const links = document.getElementsByClassName('linkToHowWorksPatient');
+  if (document.getElementsByClassName('medwatch').length) {
+    const links = document.getElementsByClassName('medwatch');
     for (let i = 0; i < links.length; i += 1) {
       links[i].addEventListener('click', e => {
         e.preventDefault();
         e.stopPropagation();
-        Enabler.exit('Gloperba', 'http://gloperba.com/');
+        Enabler.exit('Medwatch', 'https://www.fda.gov/medwatch');
+      });
+    }
+  }
+  if (document.getElementsByClassName('PInfo').length) {
+    const links = document.getElementsByClassName('PInfo');
+    for (let i = 0; i < links.length; i += 1) {
+      links[i].addEventListener('click', e => {
+        e.preventDefault();
+        e.stopPropagation();
+        Enabler.exit('PInfo', 'https://www.gloperba.com/downloads/pdf/Gloperba_PI.pdf');
+      });
+    }
+  }
+  if (document.getElementsByClassName('references').length) {
+    const links = document.getElementsByClassName('references');
+    for (let i = 0; i < links.length; i += 1) {
+      links[i].addEventListener('click', e => {
+        e.preventDefault();
+        e.stopPropagation();
+        Enabler.exit('References', 'https://www.accessdata.fda.gov/scripts/cder/ob/index.cfm');
       });
     }
   }
